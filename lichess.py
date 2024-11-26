983ce8372f030cd680a4ef4d61c6781d14975e38
echastel <156650712+echastel@users.noreply.github.com>
2024-11-26 11:24:58 +0100

    Update lichess.py
    Une fonction qui créée un tableau de donnée utilisables pour des régressions à partir des données brutes

--- lichess.py
+++ lichess.py
@@ -2,6 +2,9 @@ import requests
 
 import json
 
+import pandas as pd
+
+import statistics as sta
 
 def fetch_games(username, max_games=100):
     # URL de l'API Lichess pour récupérer les parties
@@ -48,12 +51,11 @@ if __name__ == "__main__":
 
 def tab_info_games(username, max_games):          #renvoi une tableau tab tel a que tab[i] est un tableau contenant toutesles infos intéressantes de la partie numéro i (dans l'ordre): le type de la partie (souvent blitz ou bullet), le nom d'utilisateur jouant les blancs, le nom d'utilisateur jouant les noirs, le vainqueur, la manière de gagner, elo blanc, elo noir, ouverture, les coups, les evaluations avant chaque coups, les temps  
   games_data=fetch_games(username, max_games)
-  print(games_data)
+  #print(games_data)
   tab = []
 
   for line in games_data.splitlines():
         game = json.loads(line)  # Convertir chaque ligne JSON en dictionnaire
-
         game_info = {
             'type': game.get('speed', 'Unknown'),
             'white_player': game['players']['white']['user']['name'] if 'user' in game['players']['white'] else 'Anonymous',
@@ -67,12 +69,118 @@ def tab_info_games(username, max_games):          #renvoi une tableau tab tel a 
             'evaluations': [analysis.get('eval', 'N/A') for analysis in game.get('analysis', [])],
             'clocks': game.get('clocks', [])
         }
-        
         tab.append(game_info)
     
   return tab
 
-tab_info_games("EricRosen",5)
+#print(tab_info_games("EricRosen",4))
+
+def info_player (username,max_games): #A very ugly function that gives back a panda data frame whith computable data for each game (agregated data from clock and evaluations rather than lists)
+    info=tab_info_games(username, max_games)
+    table=[]
+    for i in range(max_games):
+        table.append(list(pd.DataFrame(info[i].items())[1])) #get a table with the data from the dictionnary
+    #initiate variables
+    m_o_r=[]
+    m_m_1_r=[]
+    m_m_2_r=[]
+    m_e_1_r=[]
+    m_e_2_r=[]
+    m_o_t=[]
+    m_m_1_t=[]
+    m_m_2_t=[]
+    m_e_1_t=[]
+    m_e_2_t=[]
+    Len_game=[] 
+    won=[]
+    elo_diff=[]
+    white=[]
+
+    for i in range(max_games): #Compute the variable won that is 1 if the player won or 0 otherwise,  elo diff, the difference of elo, and white, that is 1 if the player plays first
+        if table[i][1]==username :
+            white.append(1)
+            elo_diff.append(table[i][5]-table[i][6])
+            if table[i][3]=='white' :
+                won.append(1)
+            else : won.append(0)
+        else :
+            white.append(0)
+            elo_diff.append(table[i][6]-table[i][5])
+            if table[i][3]=='black' :
+                won.append(1)
+            else : won.append(0)
+
+    for i in range(max_games): #Get two lists the times and evaluations specific to each move that the player made
+        evt2=table[i][-2]
+        cl2=table[i][-1]
+        n=len(evt2)
+        if white[i]==1 : #if player plays first get the even moves
+            evt=evt2[::2]
+            cl=cl2[::2]
+        else: #otherwise get the odd moves
+            evt=evt2[1::2]
+            cl=cl2[1::2]
+        ev= []
+        for j in range(1,len(evt)): #get the individual rating of each move
+            if not isinstance(evt[j],int) or not isinstance(evt[j-1],int) :
+                ev.append(0)
+            else :
+                ev.append(evt[j] - evt[j-1])
+
+        if len(ev)>41 : 
+            indices=[(0,10), (11,20), (21,30),(31,40),(41,n)] #split the moves in five categories : opening, middle game1, middle game 2, endgame1, endgame2 with ten move increments. Endgame2 has the remaining moves
+            means_ev=[sta.mean(ev[a:b+1]) for a,b in indices] #compute for each slice the mean rating of the move
+            means_time=[sta.mean(cl[a:b+1]) for a,b in indices] #compute for each slice the mean time taken for the move
+            Len_game.append("Long") #Get the length of the game based on the number of moves
+        elif len(ev)>31 :
+            indices=[(0,10), (11,20),(21,30),(31,n)]
+            means_ev=[sta.mean(ev[a:b+1]) for a,b in indices]
+            means_time=[sta.mean(cl[a:b+1]) for a,b in indices]
+            means_time.append('NaN') #NaN is added if there wasn't enough moves in the game to complete the last slices
+            means_ev.append('NaN')
+            Len_game.append("Medium")
+        elif len(ev)>21 :
+            indices=[(0,10), (11,20),(21,n)]
+            means_ev=[sta.mean(ev[a:b+1]) for a,b in indices]
+            means_time=[sta.mean(cl[a:b+1]) for a,b in indices]
+            means_time+=['NaN','NaN']
+            means_ev+=['NaN','NaN']
+            Len_game.append("Short")
+        else : #If the game had less than 21 moves the nalysis in terms of opening, middle game, is not useful : nothing is computed
+            means_ev=['Nan' for k in range(5)]
+            means_time=['Nan' for k in range(5)]
+            Len_game.append("Too Short")
+        m_o_r.append(means_ev[0]) #Then the computed measures are added
+        m_m_1_r.append(means_ev[1])
+        m_m_2_r.append(means_ev[2])
+        m_e_1_r.append(means_ev[3])
+        m_e_2_r.append(means_ev[4])
+        m_o_t.append(means_time[0])
+        m_m_1_t.append(means_time[1])
+        m_m_2_t.append(means_time[2])
+        m_e_1_t.append(means_time[3])
+        m_e_2_t.append(means_time[4])
+    Data = pd.DataFrame(table,columns=['type','white_player','black_player','winner','termination','white_elo','black_elo','opening','moves','evaluations','clocks'])
+    Data["white"]=white
+    Data["won"]=won
+    Data["length_game"]=Len_game
+    Data["elo_diff"]=elo_diff
+    Data["mean_opening_rating"]=m_o_r #the new colums are added to the data set
+    Data["mean_middle1_rating"]=m_m_1_r
+    Data["mean_middle2_rating"]=m_m_2_r
+    Data["mean_end1_rating"]=m_e_1_r
+    Data["mean_end2_rating"]=m_e_2_r
+    Data["mean_opening_time"]=m_o_t
+    Data["mean_middle1_time"]=m_m_1_t
+    Data["mean_middle2_time"]=m_m_2_t
+    Data["mean_end1_time"]=m_e_1_t
+    Data["mean_end2_time"]=m_e_2_t
+    Data=Data.drop(['white_player','black_player','winner','white_elo','black_elo','moves','evaluations','clocks'],axis=1) #The non computable variables are dropped from the data set
+    return Data
+
+print(info_player("EricRosen",20))
+
+
 
 username = "EricRosen"
 
@@ -92,7 +200,7 @@ def move_evaluations_list(n):
         m_list.append(u_list[i] - u_list[i-1])
     return m_list
 
-move_evaluations_list(0)
+#move_evaluations_list(0)
 
 
 def user_clocks_list(n): # n-th game
@@ -105,4 +213,4 @@ def user_clocks_list(n): # n-th game
         clocks_list = clocks_time[1::2]
     return clocks_list
 
-user_clocks_list(0)
+#user_clocks_list(0)
